4e7526312234d036c5ca67c2cf90ba426c00abc5
Omer shams <34684444+omershams@users.noreply.github.com>
2018-02-08 08:53:31 +0200

    Add files via upload

---
+++ jsweek1/week1.js
@@ -0,0 +1,25 @@
+alert("404 Error page");
+//step 1..
+let vari = "HI Honey";
+console.log(vari);
+//step 2
+let nearestWholeNumber = 4.7356;
+console.log(Math.ceil(nearestWholeNumber));
+//step 3...
+let colors =["red", "blue", "green"];
+//step 4..
+console.log(colors.length);
+
+//step 5....
+let x = "khan";
+let y = 510;
+if(x === y){
+console.log("the variable created on the js that has same datatype");
+
+}else{
+console.log("the variable created on the js they got different datatype");
+}
+//step 6
+let m = 7;
+m = m % 3;
+console.log("the is the x value= " + m);
